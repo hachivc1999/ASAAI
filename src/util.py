e4b56d07b9ff64784ddcffbec11f74e8493b1304
hachivc1999 <41011409+hachivc1999@users.noreply.github.com>
2020-06-16 23:34:46 +0900

    Update util.py

--- src/util.py
+++ src/util.py
@@ -1,59 +1,72 @@
 import random
 import string
+#This file convert useful data to desired format
 
 def jsonTags(tags):
     #convert these [('tag1',),('tag2',)] to ['tag1','tag2']
     return [tag[0] for tag in tags]
+
 def jsonId(ids):
+    #convert these [('1',),('2',)] to [1,2]
     return [int(i[0]) for i in ids]
+
 def jsonName(names):
+    #return a dictionary of names
+    #used when application wants to get name list
     namekey = ['name', 'transName']
     return [dict(zip(namekey,x)) for x in names]
+
 def jsonTitle(titleList, compatible = [], fil = []):
-    #convert list of title (with or without compatible) to json
-    #tag and season position
-    tagposdb = 9
-    seasonposdb = 8
-    tagposjson = 5
-    seasonposjson = 7
+    #the most important function
+    #convert the database format to dictionary so that it can be return as json files
+    
+    tagposdb = 9        #tag position in database format
+    seasonposdb = 8     #season position in database format
+    tagposjson = 5      #tag position in dictionary format
+    seasonposjson = 7   #season position in dictionary format
+    
     #keys
     titlekey = ['id','name', 'transName','producer','pictureLink','tags','description','seasons','favoriteCount','rank']
     seasonkey = ['name','releaseDate','numberOfEpisode','isCompleted','link']
     newtitleList = []
-    #loop for generating json
+    
+    #loop for generating dictionary
     for title in titleList:
         #remove tag and season from title
         tag = title.pop(tagposdb)
         season = title.pop(seasonposdb)
-        #remove extra ',' from tag
-        taglst = []
-        for t in tag:
-            taglst.append(t[0])
-        #insert back tag and season
-        title.insert(tagposjson, taglst)
+        
+        #insert tag
+        title.insert(tagposjson, jsonTags(tag))
+        
+        #insert season
         seasonlst = [dict(zip(seasonkey,s)) for s in season]
         title.insert(seasonposjson,seasonlst)
-        #insert compatible if required
+        
+        #insert compatible
         if compatible != []:
-            #recommend/fav
+            #the compatible is stored as a list of values matching title order
             score = compatible.pop(0)
             title.append(score)
             title = dict(zip(titlekey + ['compatibleScore'],title))
+            
+        #insert filter i.e. status code
         elif len(fil) != 0:
-            #search
             # with filter as [non,fav,watching], filter will be 0 if title not in anything
-            # if f in some array, value of it will be different. 1 for fav, 2 for non, 3 for watching and 4 for fav+watching
+            # if f in some array, value of it will be different. 0 for normal, 1 for fav, 2 for non, 3 for fav+ watching and 4 for nonfav+watching
             idtitle = title[0]
             f = round(sum([x+0.7 for x in range(0,3) if idtitle in fil[x]]))
             title.append(f)
             title = dict(zip(titlekey + ['status'], title))
+        
+        #insert nothing
         else:
             title = dict(zip(titlekey,title))
         newtitleList.append(title)
     return newtitleList
 
-#convert json to database output
 def dejsonTitle(lst):
+    #convert json to database output when the bot sends data
     titlelist = []
     for title in lst:
         titlevalue = []
@@ -67,6 +80,8 @@ def dejsonTitle(lst):
                 tags = value
             else:
                 titlevalue.append(value)
+                
+        #insert seasons and tags at the end to match database format
         titlevalue.append(seasons)
         titlevalue.append(tags)
         titlelist.append(titlevalue)
@@ -105,13 +120,14 @@ def checkMatching(dic):
         if len(date) != 3 or int(date[1]) > 12 or int(date[2]) > 31:
             return False
     return True
+
 #get a length 16 string token
 def generateToken(strlen = 16):
     return ''.join(random.choices(string.ascii_letters + string.digits, k = strlen))
 
 def searchPreprocess(name, tags, unwantedTags):
     #convert name and tags to suitable db query
-    #name: string like oni_chi
+    #name: string like 'an_apple' or 'an apple'
     #tags: string with , like "Adventure,Action"
     #unwantedTags: same as tags
     #return three lists preprocessed
